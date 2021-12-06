96fc61851ad33f9cd7ffc03266bcb5c5234d4afd
Komal-mishra-au13 <komalmishra.bca@gmail.com>
2021-12-06 21:28:13 +0530

    assignment quest 2 done

---
+++ datepicker-main/date-picker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+
+function App(){
+  return(
+    <div>
+      <DatePickerComponent placeholder="pickup"></DatePickerComponent>
+    </div>
+  )
+}
+
+export default App; 
